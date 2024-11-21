e67ae27dc7ee0289b7e829223ca676c3b2ef823e
mikghl <micael.romao32@gmail.com>
2024-11-21 11:09:55 -0300

    Construindo o componente de filtro

---
+++ src/containers/BarraLateral/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const Aside = styled.aside`
+  padding: 16px;
+  background-color: #eee;
+  height: 100vh;
+`
+export const Filtros = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 8px;
+  margin-top: 16px;
+`
+
+export const Campo = styled.input`
+  font-size: 14px;
+  font-weight: bold;
+  color: #666666;
+  padding: 8px;
+  border: 1px solid #666666;
+  border-radius: 8px;
+  background-color: #fff;
+  width: 100%;
+`
