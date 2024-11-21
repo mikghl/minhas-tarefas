132647c823497d2f9ee88af9b7cc0b8faaaf4ae2
mikghl <micael.romao32@gmail.com>
2024-11-21 11:22:23 -0300

    Parametrizando os filtros

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -6,12 +6,12 @@ const BarraLateral = () => (
     <div>
       <S.Campo type="text" placeholder="Procurar" />
       <S.Filtros>
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard ativo />
+        <FiltroCard contador={1} legenda={'pendentes'} />
+        <FiltroCard contador={2} legenda={'concluidas'} />
+        <FiltroCard contador={3} legenda={'urgentes'} />
+        <FiltroCard contador={4} legenda={'importantes'} />
+        <FiltroCard contador={5} legenda={'normal'} />
+        <FiltroCard ativo contador={10} legenda={'todas'} />
       </S.Filtros>
     </div>
   </S.Aside>
