4c352a86485823e59511d174917fd75bfcc7330e
mikghl <micael.romao32@gmail.com>
2025-01-02 10:05:04 -0300

    Filtrando Tarefas

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,20 +1,33 @@
+import { useDispatch, useSelector } from 'react-redux'
 import FiltroCard from '../../components/FiltroCard'
 import * as S from './styles'
+import { RootReducer } from '../../store'
+import { alteraTermo } from '../../store/reducers/filtro'
 
-const BarraLateral = () => (
-  <S.Aside>
-    <div>
-      <S.Campo type="text" placeholder="Procurar" />
-      <S.Filtros>
-        <FiltroCard contador={1} legenda={'pendentes'} />
-        <FiltroCard contador={2} legenda={'concluidas'} />
-        <FiltroCard contador={3} legenda={'urgentes'} />
-        <FiltroCard contador={4} legenda={'importantes'} />
-        <FiltroCard contador={5} legenda={'normal'} />
-        <FiltroCard ativo contador={10} legenda={'todas'} />
-      </S.Filtros>
-    </div>
-  </S.Aside>
-)
+const BarraLateral = () => {
+  const dispatch = useDispatch()
+  const { termo } = useSelector((state: RootReducer) => state.filtro)
+
+  return (
+    <S.Aside>
+      <div>
+        <S.Campo
+          type="text"
+          placeholder="Procurar"
+          value={termo}
+          onChange={(evento) => dispatch(alteraTermo(evento.target.value))}
+        />
+        <S.Filtros>
+          <FiltroCard contador={1} legenda={'pendentes'} />
+          <FiltroCard contador={2} legenda={'concluidas'} />
+          <FiltroCard contador={3} legenda={'urgentes'} />
+          <FiltroCard contador={4} legenda={'importantes'} />
+          <FiltroCard contador={5} legenda={'normal'} />
+          <FiltroCard ativo contador={10} legenda={'todas'} />
+        </S.Filtros>
+      </div>
+    </S.Aside>
+  )
+}
 
 export default BarraLateral
--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -5,11 +5,22 @@ import { RootReducer } from '../../store'
 
 const ListaDeTarefas = () => {
   const { itens } = useSelector((state: RootReducer) => state.tarefas)
+  const { termo } = useSelector((state: RootReducer) => state.filtro)
+
+  const filtraTarefas = () => {
+    return itens.filter(
+      (item) =>
+        item.titulo.toLocaleLowerCase().search(termo.toLocaleLowerCase()) >= 0
+    )
+  }
+
   return (
     <Container>
-      <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;ermo&ldquo;</p>
+      <p>
+        2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;{termo}&ldquo;
+      </p>
       <ul>
-        {itens.map((t) => (
+        {filtraTarefas().map((t) => (
           <li key={t.titulo}>
             <Tarefa
               descricao={t.descricao}
